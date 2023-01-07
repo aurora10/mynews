5d4863a8030269eda006dd4d7157b12d22a51678
aurora10 <aurora10@gmail.com>
2023-01-07 19:10:57 +0100

    stday

---
+++ app/NavLink.tsx
@@ -0,0 +1,23 @@
+import Link from "next/link"
+
+
+type Props = {
+    category: string
+    isActive: boolean
+}
+
+function NavLink({category, isActive}:Props) {
+  return (
+    <div>
+      < Link  href={`/news/${category}`} 
+      className={`navLink ${isActive &&
+          "underline decoration-orange-400 underline-offset-4 font-bold text-lg"
+      }`}
+      >
+          {category}
+      </Link>
+    </div>
+  )
+}
+
+export default NavLink
